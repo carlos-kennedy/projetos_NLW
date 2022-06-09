f0757d50b709af95244849e04980b8541c18918b
carlos-kennedy <carlosjustcarlo@outlook.com>
2022-06-09 13:41:15 -0300

    main

--- doctorcare/main.js
+++ doctorcare/main.js
@@ -6,6 +6,52 @@ onScroll();
 function onScroll() {
   showNavOnScroll();
   showArrowToTopButtonOnScroll();
+  activateMenuAtCurrentSection(home);
+  activateMenuAtCurrentSection(services);
+  activateMenuAtCurrentSection(about);
+  activateMenuAtCurrentSection(contact);
+}
+
+function activateMenuAtCurrentSection(section) {
+  const targetline = scrollY + innerHeight / 2;
+
+  // Verificar se a seção passou.
+  // Quais dados vou precisar?
+  const sectionTop = section.offsetTop;
+  const sectionHeight = section.offsetHeight;
+
+  const sectionTopReachOrPassedTargetline = targetline > sectionTop;
+
+  // Informações do dado
+  // console.log(
+  //   `O topo da seção chegou o passou da linha? ${sectionTopReachOrPassedTargetline}`
+  // );
+
+  // Verificar se a base está abaixo da linha alvo
+  // Quais dados vou precisar?
+
+  // A seção termina onde?
+  const sectionEndsAt = sectionTop + sectionHeight;
+
+  // O final da seção passou da linha alvo
+  const sectionEndPassedTargetline = sectionEndsAt <= targetline;
+
+  // console.log(
+  //   `O fundo da seção passou da linha? ${sectionEndPassedTargetline}`
+  // );
+
+  // Limite da seção
+  const sectionBoundaries =
+    sectionTopReachOrPassedTargetline && !sectionEndPassedTargetline;
+
+  const sectionId = section.getAttribute("id");
+
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`);
+
+  menuElement.classList.remove("active");
+  if (sectionBoundaries) {
+    menuElement.classList.add("active");
+  }
 }
 
 function showNavOnScroll() {
